a94194b758c79cadf78914334b0cd2b4dd2bf7d9
Damian Lis <foxprogramming93@gmail.com>
2021-02-20 13:06:05 +0100

    Added few lines of text in gisapia.js

--- seeds/questions/gisapia.js
+++ seeds/questions/gisapia.js
@@ -3,5 +3,10 @@ export default {
     'Cześć, bardzo mi miło, ze chcesz ze mną porozmawiać',
     'Powiedz mi jak się nazywasz?',
   ],
-  origin: ['Ja jestem z...', 'A Ty skąd jesteś?'],
+  origin: [
+    'Masz bardzo fajne imię!',
+    'Miałam dwóch znajomych, którzy tak się nazywali.',
+    'Niestety  zadnym z nich nie utrzymuje juz kontaktu...',
+    'Skąd w ogóle jesteś jezeli mogę wiedzieć?',
+  ],
 }
