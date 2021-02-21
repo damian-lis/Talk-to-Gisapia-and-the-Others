a1246035d4350767e83f4614354151bcad4f16cc
Damian Lis <foxprogramming93@gmail.com>
2021-02-21 10:33:50 +0100

    Added creation of inner-message element in messageWrapper.js

--- helpers/messageWrapper.js
+++ helpers/messageWrapper.js
@@ -1,7 +1,13 @@
 export default (message, whoTalking) => {
-  const messageWrapped = document.createElement('div')
-  messageWrapped.innerText = message
-  messageWrapped.classList.add('messageWrapper')
-  messageWrapped.classList.add(`messageWrapper-${whoTalking}`)
-  return messageWrapped
+  const messageWrapper = document.createElement('div')
+  messageWrapper.classList.add('message-wrapper')
+  //
+  const messageInner = document.createElement('div')
+  messageInner.classList.add('message-inner')
+  messageInner.classList.add(`message-inner-${whoTalking}`)
+  messageInner.innerText = message
+  //
+  messageWrapper.appendChild(messageInner)
+
+  return messageWrapper
 }
