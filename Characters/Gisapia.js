73bba2cae9d62de53b22097220131aba8cbf7da2
Damian Lis <foxprogramming93@gmail.com>
2021-02-21 23:52:40 +0100

    Deleted comment from Gisapia.js

--- Characters/Gisapia.js
+++ Characters/Gisapia.js
@@ -5,15 +5,5 @@ export default class Gisapia extends Character {
     super(questions)
     this.name = 'Gisapia'
     this.avatar = '/images/gisapia.jpg'
-    // this.memory = {}
-    // this.questions = questions ? questions : {}
   }
-
-  // getQuestions(category) {
-  //   return this.questions[category]
-  // }
-
-  // mustThink(time) {
-  //   return new Promise((resolve) => setTimeout(resolve, time))
-  // }
 }
