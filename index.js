353bc5d3624c73e9164486e81cc3b1d99731affa
Damian Lis <foxprogramming93@gmail.com>
2021-02-23 11:09:51 +0100

    Code logic has been modified to load after DOM is loaded

--- index.js
+++ index.js
@@ -7,90 +7,92 @@ import showMessenger from './helpers/showMessenger.js'
 import countTimeForTyping from './helpers/countTimeForTyping.js'
 import countTypingQuantity from './helpers/countTypingQuantity.js'
 
-const selectCharUISettings = {
-  container: '.selectCharUI-container',
-  charNames: ['Gisapia', 'Ted', 'Jessica'],
-}
-
-const selectCharUi = new SelectCharUI(selectCharUISettings)
-const charsFactory = new CharsFactory()
-const inputPanelUI = new InputPanelUI('.messenger-input-container')
-const screen = new Screen('.messenger-screen-container')
-
-//Start part
-const handleStartPart = (charName) => {
-  if (charName !== 'Gisapia') {
-    return alert('Ta postać na razie jest niedostępna. Wybierz inną!')
+document.addEventListener('DOMContentLoaded', function () {
+  const selectCharUISettings = {
+    container: '.selectCharUI-container',
+    charNames: ['Gisapia', 'Ted', 'Jessica'],
   }
 
-  selectCharUi.deleteButton('charButton')
-  const character = charsFactory.getChar(charName)
-  memory.setSelectedChar(character)
-}
+  const selectCharUi = new SelectCharUI(selectCharUISettings)
+  const charsFactory = new CharsFactory()
+  const inputPanelUI = new InputPanelUI('.messenger-input-container')
+  const screen = new Screen('.messenger-screen-container')
 
-selectCharUi.subscribe(handleStartPart, 'selectChar')
-
-//Character talking
-const handleCharTalking = async () => {
-  const character = memory.getChar()
-
-  if (!character) {
-    return alert('Wybierz rozmówcę!')
-  }
-  selectCharUi.deleteButton('startButton')
-
-  showMessenger()
-
-  const conversationStep = memory.getConversationStep()
-  const category = memory.getCategory(conversationStep)
-  const charQuestions = character.getQuestions(category)
-  if (!charQuestions) return
-
-  for (let i = 0; i < charQuestions.length; i++) {
-    const charMessage = charQuestions[i]
-
-    let timeForTyping = countTimeForTyping(charMessage.length, 80)
-    const typingQuantity = countTypingQuantity(charMessage.length)
-
-    for (let i = 0; i < typingQuantity; i++) {
-      if (i >= 1) {
-        const timeForReduceTyping = 100 * Math.floor(Math.random() * 10 + 5)
-
-        timeForTyping = timeForTyping - timeForReduceTyping
-      }
-
-      timeForTyping = timeForTyping < 1000 ? 1000 : timeForTyping
-
-      await character.mustThink(500)
-      await screen.showTyping(timeForTyping)
+  //Start part
+  const handleStartPart = (charName) => {
+    if (charName !== 'Gisapia') {
+      return alert('Ta postać na razie jest niedostępna. Wybierz inną!')
     }
 
-    const messageContainer = screen.createMessageContainer()
-    const message = screen.createMessage(charMessage, character.name)
-    const avatar = screen.createAvatar(character.avatar)
-    screen.attachToMessageContainer(messageContainer, message, avatar)
-    screen.attachToScreen(messageContainer)
+    selectCharUi.deleteButton('charButton')
+    const character = charsFactory.getChar(charName)
+    memory.setSelectedChar(character)
   }
 
-  inputPanelUI.activatePanel()
-}
+  selectCharUi.subscribe(handleStartPart, 'selectChar')
 
-selectCharUi.subscribe(handleCharTalking, 'charTalking')
+  //Character talking
+  const handleCharTalking = async () => {
+    const character = memory.getChar()
 
-//User talking
-const handleUserTalking = (userMessage) => {
-  if (userMessage === '') return alert('Musisz coś wpisać!')
+    if (!character) {
+      return alert('Wybierz rozmówcę!')
+    }
+    selectCharUi.deleteButton('startButton')
 
-  memory.setUserMessage(userMessage)
+    showMessenger()
 
-  const messageContainer = screen.createMessageContainer()
-  const message = screen.createMessage(userMessage, 'user')
-  screen.attachToMessageContainer(messageContainer, message)
-  screen.attachToScreen(messageContainer)
+    const conversationStep = memory.getConversationStep()
+    const category = memory.getCategory(conversationStep)
+    const charQuestions = character.getQuestions(category)
+    if (!charQuestions) return
 
-  memory.increaseConversationStep()
-  inputPanelUI.deactivatePanel()
-  handleCharTalking()
-}
+    for (let i = 0; i < charQuestions.length; i++) {
+      const charMessage = charQuestions[i]
 
-inputPanelUI.subscribe(handleUserTalking)
+      let timeForTyping = countTimeForTyping(charMessage.length, 80)
+      const typingQuantity = countTypingQuantity(charMessage.length)
+
+      for (let i = 0; i < typingQuantity; i++) {
+        if (i >= 1) {
+          const timeForReduceTyping = 100 * Math.floor(Math.random() * 10 + 5)
+
+          timeForTyping = timeForTyping - timeForReduceTyping
+        }
+
+        timeForTyping = timeForTyping < 1000 ? 1000 : timeForTyping
+
+        await character.mustThink(500)
+        await screen.showTyping(timeForTyping)
+      }
+
+      const messageContainer = screen.createMessageContainer()
+      const message = screen.createMessage(charMessage, character.name)
+      const avatar = screen.createAvatar(character.avatar)
+      screen.attachToMessageContainer(messageContainer, message, avatar)
+      screen.attachToScreen(messageContainer)
+    }
+
+    inputPanelUI.activatePanel()
+  }
+
+  selectCharUi.subscribe(handleCharTalking, 'charTalking')
+
+  //User talking
+  const handleUserTalking = (userMessage) => {
+    if (userMessage === '') return alert('Musisz coś wpisać!')
+
+    memory.setUserMessage(userMessage)
+
+    const messageContainer = screen.createMessageContainer()
+    const message = screen.createMessage(userMessage, 'user')
+    screen.attachToMessageContainer(messageContainer, message)
+    screen.attachToScreen(messageContainer)
+
+    memory.increaseConversationStep()
+    inputPanelUI.deactivatePanel()
+    handleCharTalking()
+  }
+
+  inputPanelUI.subscribe(handleUserTalking)
+})
