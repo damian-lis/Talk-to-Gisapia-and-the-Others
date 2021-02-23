e21ad5a9f7a41729393c6af31fbd766a0d564de4
Damian Lis <foxprogramming93@gmail.com>
2021-02-23 15:14:31 +0100

    Added new methods in Character.js

--- Characters/Character.js
+++ Characters/Character.js
@@ -1,14 +1,22 @@
 export default class Character {
-  constructor(questions) {
-    this.memory = {}
-    this.questions = questions ? questions : {}
+  constructor(messages) {
+    this.messages = messages
+    this.memoryAboutUser = {}
   }
 
-  getQuestions(category) {
-    return this.questions[category]
+  getMessagesCollection(category) {
+    return this.messages[category]
   }
 
   mustThink(time) {
     return new Promise((resolve) => setTimeout(resolve, time))
+  }
+
+  checkUserMessageInMemory(message, category) {
+    return this.memory[category].filter((word) => message.includes(word))
+  }
+
+  addToMemoryAboutUser(word, category) {
+    this.memoryAboutUser[category] = word
   }
 }
