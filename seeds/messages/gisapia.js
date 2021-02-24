adf67ff30830e176e88a51cab0302fb20f08e779
Damian Lis <foxprogramming93@gmail.com>
2021-02-24 16:35:29 +0100

    Removed comments in gisapia.js

--- seeds/messages/gisapia.js
+++ seeds/messages/gisapia.js
@@ -2,20 +2,20 @@ export default {
   introduce: {
     messages: [
       'Cześć!',
-      // 'Nazywam się Gisapia',
-      // 'Powiedz mi proszę jak mogę się do Ciebie zwracać?',
+      'Nazywam się Gisapia',
+      'Powiedz mi proszę jak mogę się do Ciebie zwracać?',
     ],
     answers: {
       isInMemory: [
         'Tak, kojarzę takie imię jak',
-        // 'Miałam dwóch znajomych, którzy tak się nazywali.',
-        // 'Niestety  zadnym z nich nie utrzymuje juz kontaktu...',
+        'Miałam dwóch znajomych, którzy tak się nazywali.',
+        'Niestety  zadnym z nich nie utrzymuje juz kontaktu...',
       ],
       isNotInMemory: [
         'Niestety nie kojarzę takiego imienia',
         'Napisz je proszę jeszcze raz, a zapamiętam!',
       ],
-      addedToMemory: ['Ale super! Ciesze się, ze poznałam nowe słowo'],
+      addedToMemory: ['Ale super! Ciesze się, ze poznałam nowe imię'],
     },
   },
   origin: {
