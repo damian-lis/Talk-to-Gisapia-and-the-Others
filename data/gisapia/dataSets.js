37f5cc51c785f981aec3cbb515f030285213c04f
Damian Lis <foxprogramming93@gmail.com>
2021-02-25 19:18:54 +0100

    Added new elements in memorySets and dataSets of gisapia

--- data/gisapia/dataSets.js
+++ data/gisapia/dataSets.js
@@ -35,11 +35,28 @@ export default [
   },
 
   {
+    category: 'hobby',
+    messages: ['Powiedz mi jakie masz hobby?'],
+    answers: {
+      isInMemory: ['znam to zajęcie jak własną kieszeń!', 'Bla bla'],
+      isNotInMemory: [
+        'Hmmm ciekawe... Nie kojarzę takiego hobby.',
+        'Mozesz jeszcze raz je napisać to zapamiętam?',
+      ],
+      addedToMemory: [
+        'wlicza się do ekstremalnych sportów, czy coś mylę? :)',
+        'Nie wazne poczytam napewno!',
+      ],
+    },
+  },
+
+  {
     category: 'summary',
     messages: [
       'Okej to co udało mi się zapamiętać odnośnie Ciebie',
       'Masz na imię ',
       'Pochodzisz z ',
+      'Twoje hobby to ',
       'Ciesze się, ze mogłam Cię poznać!',
       'Niestety ze względu na inne obowiazki muszę zmykać...',
       'Jezeli chcesz aby Twoje odpowiedzi trafiły do mojej pamięci na zawsze to napisz proszę "zapisz" :)',
