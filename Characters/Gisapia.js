21691699433ae475853f6826040f7c82b27b3c32
Damian Lis <foxprogramming93@gmail.com>
2021-02-26 13:46:01 +0100

    Shifted svg file to images folder and renamed image files

--- Characters/Gisapia.js
+++ Characters/Gisapia.js
@@ -5,6 +5,6 @@ export default class Gisapia extends Character {
   constructor(dataSets, memorySets) {
     super(dataSets, memorySets)
     this.name = 'Gisapia'
-    this.avatar = '/images/gisapia.jpg'
+    this.avatar = '/images/gisapia/avatar.jpg'
   }
 }
