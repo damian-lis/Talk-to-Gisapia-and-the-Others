acd81df7ea5548d91e4c51e082edd919243b6b17
Damian Lis <foxprogramming93@gmail.com>
2021-02-28 12:55:56 +0100

    Created NullCharacter.js and implemented it to CharsFactory.js

---
+++ Characters/NullCharacter.js
@@ -0,0 +1,5 @@
+export default class NullCharacter {
+  constructor() {
+    this.name = null
+  }
+}
--- objects/CharsFactory.js
+++ objects/CharsFactory.js
@@ -1,16 +1,20 @@
 import Gisapia from '../characters/Gisapia.js'
+import NullCharacter from '../characters/NullCharacter.js'
 import { scriptTalk, memory } from '../data/gisapia/index.js'
 import { charNames } from '../data/globalNames.js'
 
 export default class CharsFactory {
   constructor() {
     this.gisapia = new Gisapia(scriptTalk, memory)
+    this.nullCharacter = new NullCharacter()
   }
 
   getChar(charName) {
     switch (charName) {
       case charNames.gisapia:
         return this.gisapia
+      default:
+        return this.nullCharacter
     }
   }
 }
