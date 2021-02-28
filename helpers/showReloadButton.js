56cb9c1a4c760887abc89238b4f5e0fb2abbd680
Damian Lis <foxprogramming93@gmail.com>
2021-02-28 14:36:16 +0100

    Deleted showReloadButton.js

--- helpers/showReloadButton.js
+++
@@ -1,8 +0,0 @@
-import { communiques } from '../data/globalNames.js'
-
-export default (callback) => {
-  const button = document.createElement('button')
-  button.innerText = communiques.talkAgain
-  button.addEventListener('click', () => callback())
-  document.body.appendChild(button)
-}
