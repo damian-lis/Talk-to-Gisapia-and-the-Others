d0d3444b9b27789594a96d65d8dcbf46596dab23
Damian Lis <foxprogramming93@gmail.com>
2021-02-28 14:38:55 +0100

    Deleted comuniques from globalNames.js

--- data/globalNames.js
+++ data/globalNames.js
@@ -28,13 +28,6 @@ export const buttons = {
   },
 }
 
-export const communiques = {
-  characterDoesntExist: 'Ta postać na razie jest niedostępna. Wybierz inną!',
-  selectChar: 'Wybierz rozmówcę!',
-  mustToWriteSomething: 'Musisz coś napisać',
-  talkAgain: 'Porozmawiaj ponownie',
-}
-
 export const subscriberTypes = {
   selectChar: 'selectChar',
   charTalking: 'charTalking',
