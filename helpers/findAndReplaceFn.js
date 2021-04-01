5f4396a700fabb79c5597bdfaa89af8b30bb4cb4
Damian Lis <foxprogramming93@gmail.com>
2021-04-01 21:51:31 +0200

    Changed parameters name in findAndReplaceFn.js to be more intuitive

--- helpers/findAndReplaceFn.js
+++ helpers/findAndReplaceFn.js
@@ -1,9 +1,9 @@
 export default ({ wordsSets }, messages) => {
   return messages.map((message) => {
     wordsSets.forEach((wordSet) => {
-      if (message.includes(wordSet.toFind)) {
-        const regexp = new RegExp(wordSet.toFind, 'gi')
-        message = message.replace(regexp, wordSet.toReplace)
+      if (message.includes(wordSet.search)) {
+        const regexp = new RegExp(wordSet.search, 'gi')
+        message = message.replace(regexp, wordSet.replace)
       }
     })
 
