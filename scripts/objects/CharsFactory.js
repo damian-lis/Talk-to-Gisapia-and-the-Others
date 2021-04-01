f0babf2326f255ac8c38ba5d797be3a5d270d69d
Damian Lis <foxprogramming93@gmail.com>
2021-04-01 22:34:28 +0200

    Code improved for better readability in CharsFactory.js

--- scripts/objects/CharsFactory.js
+++ scripts/objects/CharsFactory.js
@@ -1,33 +1,29 @@
 import { Gisapia, NullCharacter, Hookin, Reduxon } from './index.js'
-import { scriptTalk, memory } from '../../data/gisapia/index.js'
-import {
-  scriptTalk as tedScriptTalk,
-  memory as tedMemory,
-} from '../../data/hookin/index.js'
-import {
-  scriptTalk as reduxonScriptTalk,
-  memory as reduxonMemory,
-} from '../../data/reduxon/index.js'
+import { gisapiaScriptTalk, gisapiaMemory } from '../../data/gisapia/index.js'
+import { hookinScriptTalk, hookinMemory } from '../../data/hookin/index.js'
+import { reduxonScriptTalk, reduxonMemory } from '../../data/reduxon/index.js'
 import { charNames } from '../../data/globalNames.js'
 
-export default class CharsFactory {
+class CharsFactory {
   constructor() {
-    this.gisapia = new Gisapia(scriptTalk, memory)
-    this.hookin = new Hookin(tedScriptTalk, tedMemory)
+    this.gisapia = new Gisapia(gisapiaScriptTalk, gisapiaMemory)
+    this.hookin = new Hookin(hookinScriptTalk, hookinMemory)
     this.reduxon = new Reduxon(reduxonScriptTalk, reduxonMemory)
     this.nullCharacter = new NullCharacter()
   }
 
   getChar(charName) {
     switch (charName) {
-      case charNames.gisapia:
+      case charNames.Gisapia:
         return this.gisapia
-      case charNames.hookin:
+      case charNames.Hookin:
         return this.hookin
-      case charNames.reduxon:
+      case charNames.Reduxon:
         return this.reduxon
       default:
         return this.nullCharacter
     }
   }
 }
+
+export default CharsFactory
