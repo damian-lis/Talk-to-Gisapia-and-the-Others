a87ff98fb846c52d0d77089b238b152f6518beac
Damian Lis <foxprogramming93@gmail.com>
2021-04-17 14:54:59 +0200

    Added the option to restore the original class in setClassesFn.js

--- scripts/helpers/setClassesFn.js
+++ scripts/helpers/setClassesFn.js
@@ -1,9 +1,11 @@
 export default (objects) => {
   objects.map((object) => {
+    console.log(`.${object.element}`)
     let element = object.element
     if (typeof object.element === 'string') {
-      element = document.querySelector(object.element)
+      element = document.querySelector(`.${object.element}`)
     }
+    element.className = object.element
     element.classList.add(...object.classes)
   })
 }
