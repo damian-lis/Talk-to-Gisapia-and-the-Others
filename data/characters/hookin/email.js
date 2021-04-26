5a21df30c45c7f3cf43c33a74b66255ba22241d2
Damian Lis <foxprogramming93@gmail.com>
2021-04-26 19:55:51 +0200

    Added a strong tag to email messages

--- data/characters/hookin/email.js
+++ data/characters/hookin/email.js
@@ -6,12 +6,12 @@ export default {
             <p> Hej -userName- tutaj Hookin! </p>
             <div style="height:5px"></div>
             <p>Zgodnie z obietnicą przesyłam informacje jakie udało mi się zapamiętać. 😉</p>
-            <p>Więc tak, masz na imię -userName- ,</p>
-            <p>Twoja miejscowość zamieszkania to -userOrigin- </p>
-            <p>hobby jakie uprawiasz w wolnym czasie to -userHobby- </p>
-            <p>Chyba dobrze się spisałam co nie? 😃</p>
+            <p>Więc tak, masz na imię <strong>-userName-</strong> ,</p>
+            <p>Twoja miejscowość zamieszkania to <strong>-userOrigin-</strong> </p>
+            <p>hobby jakie uprawiasz w wolnym czasie to <strong>-userHobby-</strong> </p>
+            <p>Chyba dobrze się spisałem co nie? 😃</p>
             <div style="height:5px"></div>
-            <p>Dzięki wielkie za rozmowę, dobrze się bawiłam! 😎</p>
+            <p>Dzięki wielkie za rozmowę, dobrze się bawiłem! 😎</p>
             <p>Mam nadzieję, że Ty również 😋!</p>
             `,
   },
@@ -22,9 +22,9 @@ export default {
             <p> Hey -userName-, this is Hookin! </p>
             <div style="height:5px"></div>
             <p>As promised, I am sending information that I can remember. 😉</p>
-            <p>So yeah, Your name is -userName-,</p>
-            <p>Your city of residence is -userOrigin-,</p>
-            <p>the hobby You do in Your spare time is -userHobby-, </p>
+            <p>So yeah, Your name is <strong>-userName-</strong>,</p>
+            <p>Your city of residence is <strong>-userOrigin-</strong>,</p>
+            <p>the hobby You do in Your spare time is <strong>-userHobby-</strong>, </p>
             <p>I think that I did well did not? 😃</p>
             <div style="height:5px"></div>
             <p>Thanks so much for the interview, had a good time! 😎</p>
