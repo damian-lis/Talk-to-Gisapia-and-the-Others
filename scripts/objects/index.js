0f98a6a9d7a3180e5da68449afa1de89c63f24d2
Damian Lis <foxprogramming93@gmail.com>
2021-05-02 12:40:46 +0200

    Created messenger folder and moved here all messenger classes

--- scripts/objects/index.js
+++ scripts/objects/index.js
@@ -1,10 +1,12 @@
+import {
+  Messenger,
+  MessengerInterface,
+  MessengerScreen,
+} from './messenger/index.js'
 import Background from './Background.js'
 import GisapiaAnimation from './GisapiaAnimation.js'
 import memory from './Memory.js'
 import CharsFactory from './CharsFactory.js'
-import Messenger from './Messenger.js'
-import MessengerInterface from './MessengerInterface.js'
-import MessengerScreen from './MessengerScreen.js'
 import SelectCharUI from './SelectCharUI.js'
 import Character from './Character.js'
 import Gisapia from './characters/Gisapia.js'
---
+++ scripts/objects/messenger/index.js
@@ -0,0 +1,5 @@
+import Messenger from './Messenger.js'
+import MessengerInterface from './MessengerInterface.js'
+import MessengerScreen from './MessengerScreen.js'
+
+export { Messenger, MessengerInterface, MessengerScreen }
